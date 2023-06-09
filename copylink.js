a5ffbfd8f77edb8a7d7c5ffca545dc13a13700b5
Nayaknaina <117939981+Nayaknaina@users.noreply.github.com>
2023-06-09 07:51:32 +0530

    Add files via upload

---
+++ copylink.js
@@ -0,0 +1,7 @@
+let newURL=document.getElementById("shorturl");
+let copybutton=document.getElementById("copy");
+
+copybutton.onclick=()=>{
+    newURL.select();
+    window.navigator.clipboard.writeText(newURL.value);
+}
